30581a751524f56c2ee02e14ad316f0222da5e75
leader22 <y.sugiura.0316@gmail.com>
2016-01-20 13:39:14 +0900

    misc

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,6 @@
 'use strict';
 import * as ReactDOM from 'react-dom';
+import objectAssign from 'object-assign';
 import App from './components/app.jsx';
 import PartsModel from './models/parts';
 
@@ -13,6 +14,7 @@ const app = new App({
     showFixModal: false
   },
 
+  // TODO: あとでけす
   middlewares: [
     (state) => {
       console.info(state);
@@ -21,9 +23,11 @@ const app = new App({
   ]
 });
 
-PartsModel.fetchAll().then(() => {
-  app.update((state) => {
-    state.settings = PartsModel.getDefaultSettings();
-    return state;
+global.addEventListener('load', () => {
+  PartsModel.fetchAll().then(() => {
+    app.update((state) => {
+      state.settings = PartsModel.getDefaultSettings();
+      return objectAssign({}, state);
+    });
   });
-});
+}, false);
