60791e4291d689affc6ffc61a8c1dee42aa35a4f
leader22 <y.sugiura.0316@gmail.com>
2016-01-20 12:08:24 +0900

    Keep state immutable

--- src/script/components/app.jsx
+++ src/script/components/app.jsx
@@ -1,6 +1,7 @@
 'use strict';
 import * as React from 'react'; // eslint-disable-line no-unused-vars
 import {Flux} from 'flumpt';
+import objectAssign from 'object-assign';
 import Header from './header.jsx';
 import Preview from './preview.jsx';
 import ToolPanel from './tool-panel.jsx';
@@ -11,28 +12,28 @@ export default class extends Flux {
     this.on('set:color', ({target, colorId}) => {
       this.update((state) => {
         state.settings[`${target}Color`] = colorId;
-        return state;
+        return objectAssign({}, state);
       });
     });
 
     this.on('set:type', ({target, typeId}) => {
       this.update((state) => {
         state.settings[`${target}Type`] = typeId;
-        return state;
+        return objectAssign({}, state);
       });
     });
 
     this.on('show:fixModal', () => {
       this.update((state) => {
         state.showFixModal = true;
-        return state;
+        return objectAssign({}, state);
       });
     });
 
     this.on('hide:fixModal', () => {
       this.update((state) => {
         state.showFixModal = false;
-        return state;
+        return objectAssign({}, state);
       });
     });
   }
