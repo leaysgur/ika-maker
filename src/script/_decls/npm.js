bbd2b75f48c878eaae2f97622e7ccc4c7a2d9b2c
leader22 <y.sugiura.0316@gmail.com>
2016-06-21 20:09:32 +0900

    type main

--- src/script/_decls/npm.js
+++ src/script/_decls/npm.js
@@ -1,3 +1,7 @@
 declare module 'react-tap-event-plugin' {
   declare function exports(): Function;
 }
+
+declare module 'object-assign' {
+  declare function exports<A, B>(a: A, b: B): A & B;
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,4 @@
+// @flow
 'use strict';
 import * as ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
