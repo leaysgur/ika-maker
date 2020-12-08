5dc02265be33e3cae0aa0850d98ea9534336f852
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-12-08 18:25:37 +0900

    Remove cache impl

--- src/domain/core.js
+++ src/domain/core.js
@@ -67,8 +67,6 @@ const $canvas = document.createElement("canvas");
 $canvas.width = $canvas.height = IMG_SIZE;
 const ctx = $canvas.getContext("2d");
 
-const imgCache = {};
-
 export const generateFixImgSrcBySettings = async (scheme, settings) => {
   const imgPathArr = [
     getImgPath(scheme, "bg", settings.bgType, settings.bgColor),
@@ -91,16 +89,10 @@ export const generateFixImgSrcBySettings = async (scheme, settings) => {
   const imgRefs = {};
   await Promise.all(imgPathArr.map(([partsId, path]) => {
     return new Promise((resolve, reject) => {
-      if (imgCache[path]) {
-        imgRefs[partsId] = imgCache[path];
-        return resolve();
-      }
-
       const img = new Image();
       img.src = path;
       img.onload = () => {
         imgRefs[partsId] = img;
-        imgCache[path] = img;
         resolve();
       };
       img.onerror = img.onabort = reject;
